03d3ac9adaee85a4e56a7b47176f66b025ed758d
NDCFL <275300091@qq.com>
2017-10-19 12:12:21 +0800

    ssh+ueditor+mysql实现的文章资讯的增删改查功能

---
+++ SSHUeditor/src/main/java/com/ht/dao/UeditorDAO.java
@@ -0,0 +1,9 @@
+package com.ht.dao;
+
+import com.ht.pojo.Ueditor;
+
+/**
+ * Created by chenfeilong on 2017/10/19.
+ */
+public interface UeditorDAO  extends  BaseDAO<String,Ueditor> {
+}
---
+++ SSHUeditor/src/main/java/com/ht/service/UeditorService.java
@@ -0,0 +1,10 @@
+package com.ht.service;
+
+import com.ht.dao.BaseDAO;
+import com.ht.pojo.Ueditor;
+
+/**
+ * Created by chenfeilong on 2017/10/19.
+ */
+public interface UeditorService extends BaseDAO<String,Ueditor> {
+}
